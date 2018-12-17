fbf0124111666a662406d32207dcf968f25d53d7
Wei-LinHsiao <36719288+Wei-LinHsiao@users.noreply.github.com>
2018-12-17 13:19:08 +0800

    Add logisc regression feature, basic summary statistics

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import os
 import re
 import string
 import numpy as np
+import sklearn as sk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 # Global variables
 output_dir = "data"
@@ -38,17 +42,87 @@ label = np.array([])
 index = 0
 # Create corpus
 # Key is number in lookup, value is text for repsctive comment
-corpus = {}
+corpus_raw = {}
 
 # Process all the above
 for comment_id in data_demo:
     lookup[index] = comment_id
     label = np.append(label, 0)
-    corpus[index] = data_demo[comment_id]["text"].encode('ascii')
+    corpus_raw[index] = data_demo[comment_id]["text"].encode('ascii')
     index += 1
 for comment_id in data_repub:
     lookup[index] = comment_id
     label = np.append(label, 1)
-    corpus[index] = data_repub[comment_id]["text"].encode('ascii')
+    corpus_raw[index] = data_repub[comment_id]["text"].encode('ascii')
     index += 1
 
+# Create corpus of TF-IDF vectors
+# Corpus is a sparse matrix; each row is a different document
+tfidf = TfidfVectorizer(stop_words = 'english', ngram_range = (1, 2))
+corpus = tfidf.fit_transform(corpus_raw.values())
+
+# Get a test-train split
+comment_train, comment_test, label_train, label_test = train_test_split(corpus, label, test_size=0.2)
+
+
+# Run logistic regression, get coefficents
+model = LogisticRegression(penalty = "l2", solver = "lbfgs", max_iter = 100, fit_intercept = False)
+model.fit(comment_train, label_train)
+coef = model.coef_[0]
+
+# Get label for testing data
+label_test_pred = np.matmul(comment_test.toarray(), coef)
+
+# Get summary statistics on prediction
+def calculate_f1(true, predicted, threshold = 0):
+    """
+    Calculates the F1 score
+
+    :param true: the actual labels
+    :param predicted: predicted labels
+    :return: returns nothing, prints out F1 score
+    """
+    # Threshold the values
+    predicted[predicted > threshold] = 1
+    predicted[predicted <= threshold] = 0
+    tp = 0.0
+    tn = 0.0
+    fp = 0.0
+    fn = 0.0
+
+    for idx in range(0, len(true)):
+        # tp: both positive
+        if true[idx] == 1 and predicted[idx] == 1:
+            tp += 1
+        # tn; both negative
+        elif true[idx] == 0 and predicted[idx] == 0:
+            tn += 1
+        # fp: predicted 1 but 0
+        elif true[idx] == 0 and predicted[idx] == 1:
+            fp += 1
+        else:
+            fn += 1
+
+    # Get f1 score
+    recall = tp / (tp + fn)
+    precision = tp / (tp + fp)
+
+    f1 = 2 / ((1 / recall) + (1 / precision))
+    print f1
+    print tp
+    print tn
+    print fp
+    print fn
+calculate_f1(label_test, label_test_pred)
+
+
+# Get the 20 highest and 20 lowest coefficents
+
+
+
+
+# feature_names = tfidf.get_feature_names()
+# corpus_index = [n for n in corpus_raw]
+# rows, cols = corpus.nonzero()
+# #for row, col in zip(rows, cols):
+# #    print((feature_names[col], corpus_index[row]), corpus[row, col])
